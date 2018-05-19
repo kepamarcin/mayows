574cac065caa76bc6846764eb40f00002a80f1b0
pinmar <pinmar1980@outlook.com>
2018-05-19 18:11:30 +0200

    no polyfill

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import navigation from "./navigation";
 import lazy from "./lazy";
-import polyfill from "./evtpolyfill";
+// import polyfill from "./evtpolyfill";
 // import scroll from "./scroll";
 
