11f3ff890c7dec16f72806294799d35838a4aec2
pinmar <pinmar1980@outlook.com>
2018-06-13 19:03:35 +0200

    js refactor ev liteners

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import navigation from "./navigation";
 import lazy from "./lazy";
+// import lazy from "./js";
 // import polyfill from "./evtpolyfill";
 // import scroll from "./scroll";
 
--- src/js/js.js
+++ src/js/js.js
@@ -6,7 +6,6 @@ var lazy;
     }
     let btn = document.getElementById("buy"),
          data = document.querySelector(".buy-data");
-
     if(btn){
       btn.addEventListener("click", showData, false);
     }
@@ -14,6 +13,8 @@ var lazy;
     const bgOverlay = document.querySelector(".background-overlay");
     let bioBtn = document.querySelectorAll(".contact .product-button");
     let arr = Array.from(bioBtn);
+
+
     const body = document.querySelector("body");
     const wrapper = document.querySelector(".wrapper-people");
 
@@ -21,15 +22,13 @@ var lazy;
       wrapper.addEventListener("click", function(e){
         const productButtons = wrapper.querySelectorAll("button");
         if( e.target.className === "button margin-top"){
-          // open.bind(e.target.parentElement.nextElementSibling)();
           open.bind(e.target.parentElement.nextElementSibling)();
           
         }
         else if(e.keyCode == 27 || e.target.classList.contains("background-overlay") || e.target.classList.contains("red")){
           
           close.bind(this)();
-          // let qq = document.querySelector(".bio-details");
-          // qq.removeEventListener("scroll", handler, false);
+          
         }
         else 
           return;
@@ -42,48 +41,43 @@ var lazy;
     function removeNoScroll(){
       body.classList.remove("no-scroll");
     }
-    function open(){
-      
+    let open = function(){
+      addNoScroll();
       this.classList.add("show-bio");
       bgOverlay.classList.add("background-visible");
       window.addEventListener("keydown", close, false);
-
       createCloseBtn.bind(this)();
       loadImage.bind(this)();
-      addNoScroll();
-      scrollEvent();
     }
 
-    function close(){
-      
+    let close = function(){
+      removeNoScroll();
       let panelOpened = document.querySelector(".show-bio");
-      panelOpened.removeEventListener("scroll", handler, false);
       if(panelOpened){
         panelOpened.classList.remove("show-bio");
         setTimeout(function(){
           panelOpened.scrollTop = 0;
         }, 500);
       }
-      
       bgOverlay.classList.remove("background-visible");
       window.removeEventListener("keydown", close, false);
-      removeNoScroll();
-      
+      panelOpened.removeEventListener("scroll", positionForCloseBtn, false);
     }
-    function createCloseBtn(){
+
+    let createCloseBtn = function(){
       if(this.querySelector(".red")){
         return;
       }
-      
       const closeBtn = document.createElement('span');
-      const bioWrapper = this.firstElementChild;
+      const bioWrapper = this.querySelector(".bio-wrapper");
       closeBtn.innerHTML = "Close";
       closeBtn.classList = "button red button-sticky";
       
-      bioWrapper.appendChild(closeBtn);
+      this.addEventListener("scroll", positionForCloseBtn.bind(this), false);
+      this.firstElementChild.appendChild(closeBtn);
     }
 
-    function loadImage(){
+    let loadImage = function(){
       let img = this.firstElementChild.firstElementChild;
       if(img.getAttribute("data-src") == img.getAttribute("src")){
         return;
@@ -91,31 +85,16 @@ var lazy;
       img.setAttribute("src", img.getAttribute("data-src"));
     }
 
-    var handler;
-    function scrollEvent(){
-
-      var bioD = document.querySelector(".bio-details");
-
-      handler = function(){
-        let divTop = this.firstElementChild.getBoundingClientRect().top;
-        let actualTop = (-1)*divTop + 10;
-        let closeBtn = this.firstElementChild.querySelector(".button");
-        closeBtn.style.top = actualTop +"px";
-      }
-
-      bioD.addEventListener("scroll", handler, false);
-
+    let positionForCloseBtn = function(){
+      let divTop = this.firstElementChild.getBoundingClientRect().top;
+      let actualTop = (-1)*divTop + 10;
+      let closeBtn = this.firstElementChild.querySelector(".button");
+      closeBtn.style.top = actualTop +"px";
     }
-
     // function positionForCloseBtn(){
-      
     //   let divTop = this.firstElementChild.getBoundingClientRect().top;
     //   let actualTop = (-1)*divTop + 10;
-    //   let closeBtn = this.firstElementChild.querySelector(".button");
     //   closeBtn.style.top = actualTop +"px";
-    //   // this.addEventListener("scroll", positionForCloseBtn.bind(this), false);
-    //   qwe(e);
-      
     // }
 
     
